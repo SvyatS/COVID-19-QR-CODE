c3a626902e9dcbd218f11a17363943054d31dcef
SvyatS <prisenko2002@gmail.com>
2021-11-13 18:44:22 +0700

    added HttpResponseNotFound

--- vaccine/core/views.py
+++ vaccine/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponseNotFound
 from .models import UserData
 from .utils import *
 
@@ -7,7 +8,9 @@ from .utils import *
 class VaccinePageView(View):
     @staticmethod
     def get(request, pk):
-        user = UserData.objects.get(id=pk)
-        content = vaccine_structure(user)
-        print(content)
-        return render(request, 'vaccinePage.html', content)
+        try:
+            user = UserData.objects.get(id=pk)
+            content = vaccine_structure(user)
+            return render(request, 'vaccinePage.html', content)
+        except Exception as e:
+            return HttpResponseNotFound('<h1>User not found</h1>')
